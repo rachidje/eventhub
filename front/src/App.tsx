48853c77e522bcf0dfdc88548bd2c4094c9b0866
rachidWassati <rachid.jeffali@wassati.com>
2025-07-31 23:44:38 +0200

    Workflow: inclus les tests unitaires du front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,6 @@
 import { AppWrapper } from './modules/base/ui/AppWrapper'
 import { Layout } from './modules/base/ui/Layout'
-import { CreateEventPage } from './modules/create-event/ui/pages/CreateEventPage'
+// import { CreateEventPage } from './modules/create-event/ui/pages/CreateEventPage'
 import { RegisterForm } from './modules/register-user/ui/pages/RegisterForm'
 
 function App() {
